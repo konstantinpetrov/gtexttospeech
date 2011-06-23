cc29f4db404d453128bf4129fb6e9fe2026bafb5
Paul Miller <paulmillr@me.com>
2011-06-23 15:30:03 +0300

    Code style updated.

--- gtexttospeech/main.py
+++ gtexttospeech/main.py
@@ -10,23 +10,23 @@ from optparse import OptionParser
 
 
 def main():
-    usage = 'Usage: %prog [options] arg1 arg2'
-    parser = OptionParser(prog='gtexttospeech', usage=usage,
-                          version='%prog 0.1')
-    parser.add_option('-f', '--file', dest='filename', type='str', default='',
-                      help='file, that contains phrases')
-    parser.add_option('-o', '--outfile', dest='outfile', default='',
-                      help='output mp3 file')
-    parser.add_option('-t', '--text', dest='text',
-                      help='phrase')
-    parser.add_option('-l', '--language', dest='language',
-                      help='phrase language (ru etc.)', default='ru')
-    parser.add_option('-v', '--verbose',
-                      action='store_true', dest='verbose', default=False,
-                      help='be verbose')
+    usage = "Usage: %prog [options] arg1 arg2"
+    parser = OptionParser(prog="gtexttospeech", usage=usage,
+                          version="%prog 0.1")
+    parser.add_option("-f", "--file", dest="filename", type="str", default="",
+                      help="file, that contains phrases")
+    parser.add_option("-o", "--outfile", dest="outfile", default="",
+                      help="output mp3 file")
+    parser.add_option("-t", "--text", dest="text",
+                      help="phrase")
+    parser.add_option("-l", "--language", dest="language",
+                      help="phrase language (ru etc.)", default="ru")
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="verbose", default=False,
+                      help="be verbose")
     (options, args) = parser.parse_args()
     if sys.stdin:
-        lang = args[0] if args else 'ru'
+        lang = args[0] if args else "ru"
         TextToSpeech(sys.stdin, language=lang).create(sys.stdout)
     else:
         parser.print_help()
